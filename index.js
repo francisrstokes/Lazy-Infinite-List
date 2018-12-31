39ec89b1a9918a4792fed4dc0835c3f03e774a08
Frank Stokes <frank@wearereasonablepeople.com>
2018-12-31 11:55:33 +0100

    Adds HM to the code

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+// data Infinite a
 function Infinite(gen) {
   this.gen = gen;
   this.transformations = [];
 };
 
+// _copy :: Infinite a ~> Infinite a
 Infinite.prototype._copy = function () {
   const newInfinite = Infinite.of(this.gen);
   newInfinite.transformations = this.transformations.slice();
   return newInfinite;
 };
 
+// _transform :: (String, Function) -> Infinite a ~> Infinite b
 Infinite.prototype._transform = function (type, fn) {
   const newInfinite = this._copy();
   newInfinite.transformations.push({ type, fn });
   return newInfinite;
 };
 
+// zip :: Infinite a ~> Infinite b -> Infinite [a, b]
 Infinite.prototype.zip = function (inf) {
   if (!(inf instanceof Infinite)) {
     throw new Error(`Argument to zip must be another Infinite`);
@@ -22,27 +26,35 @@ Infinite.prototype.zip = function (inf) {
   return this._transform('zip', inf.gen);
 };
 
+// map :: Infinite a ~> (a -> b) -> Infinite b
 Infinite.prototype['fantasy-land/map'] = Infinite.prototype.map = function (fn) {
   return this._transform('map', fn);
 };
+
+// filter :: Infinite a ~> (a -> Bool) -> Infinite b
 Infinite.prototype['fantasy-land/filter'] = Infinite.prototype.filter = function (fn) {
   return this._transform('filter', fn);
 };
+
+// filterDependent :: Infinite a ~> (a -> [a] -> Bool) -> Infinite b
 Infinite.prototype.filterDependent = function (fn) {
   return this._transform('filterDependent', fn);
 };
 
+// take :: Infinite a ~> Integer -> [a]
 Infinite.prototype.take = function (n) {
   const iterator = this.gen();
   return interpret(iterator, n, this.transformations);
 };
 
+// nth :: Infinite a ~> Integer -> a
 Infinite.prototype.nth = function (n) {
   const iterator = this.gen();
   const res = interpret(iterator, n, this.transformations);
   return res[n-1];
 };
 
+// interpret :: Iterator a -> Integer -> [a]
 const interpret = (iterator, n, transformations) => {
   let last;
   let out = new Array(n);
@@ -55,32 +67,32 @@ const interpret = (iterator, n, transformations) => {
     last = value;
     if (done) break;
 
-    let skip = false;
-
+    let filtered = false;
     let x = value;
     for (let ti = 0; ti < transformations.length; ti++) {
       if (transformations[ti].type === 'map') {
         x = transformations[ti].fn(x);
       } else if (transformations[ti].type === 'filter' && !transformations[ti].fn(x)) {
-        skip = true;
-        break;
+        filtered = true;
       } else if (transformations[ti].type === 'filterDependent' && !transformations[ti].fn(x, out.slice(0, i))) {
-        skip = true;
-        break;
+        filtered = true;
       } else if (transformations[ti].type === 'zip') {
         x = [x, zips[ti].next().value];
       }
+
+      if (filtered) break;
     }
 
-    if (!skip) {
+    if (!filtered) {
       out[i++] = x;
     }
-    skip = false;
+    filtered = false;
   }
 
   return out;
 };
 
+// of :: Generator a -> Infinite a
 Infinite['fantasy-land/of'] = Infinite.of = gen => new Infinite(gen);
 
 module.exports = Infinite;
